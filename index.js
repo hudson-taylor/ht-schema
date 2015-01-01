32231178b908295e3a25a3ead0e98b18f1ee3865
Adam Brady <adam@boxxen.org>
2015-01-01 22:33:31 +1100

    clean up
    Signed-off-by: Adam Brady <adam@boxxen.org>

--- index.js
+++ index.js
@@ -22,36 +22,41 @@ validators.makeParser = makeParser;
 module.exports = validators;
 
 function makeParser(parserFunc, docFunc) {
-    //parserFunc takes arguments, child-validators || null, and the data to
-    //parse, it should throw an Error if the data is invalid, containing a
-    //reason. Otherwise it should return a value. This value can be mutated,
-    //it will be the "validated" value.
+
+    // parserFunc takes arguments, child-validators || null, and the data to
+    // parse, it should throw an Error if the data is invalid, containing a
+    // reason. Otherwise it should return a value. 
+    // This value can be mutated, it will be the "validated" value.
 
     return function validator(args, childValidators) {
-        //overly complex method of managing argument order!
+
+        // Overly complex method of managing argument order!
         switch(arguments.length) {
-            case 0:
+
+            case 0: {
                 childValidators = {};
                 args = {};
                 break;
-            case 1:
-                //one argument, arge they args or validators?
+            }
+
+            case 1: {
+
+                // One argument, are they args or validators?
                 var areValidators = false;
+
                 if(Array.isArray(arguments[0])) {
-                    for(var i=0; i<arguments[0].length; i++) {
-                         if(typeof arguments[0][i] == "object" &&
-                                arguments[0][i].hasOwnProperty("childValidators")){
-                                    areValidators = true;
-                                    break;
-                                }
+                    for(var i = 0; i < arguments[0].length; i++) {
+                        if(typeof arguments[0][i] == "object" && arguments[0][i].hasOwnProperty("childValidators")) {
+                            areValidators = true;
+                            break;
+                        }
                     }
                 } else {
                     for(var k in arguments[0]) {
-                        if(typeof arguments[0][k] == "object" &&
-                                arguments[0][k].hasOwnProperty("childValidators")){
-                                    areValidators = true;
-                                    break;
-                                }
+                        if(typeof arguments[0][k] == "object" && arguments[0][k].hasOwnProperty("childValidators")) {
+                            areValidators = true;
+                            break;
+                        }
                     }
                 }
 
@@ -63,18 +68,20 @@ function makeParser(parserFunc, docFunc) {
                     childValidators = null;
                 }
                 break;
+
+            }
+            
         }
 
-        return new (function(args, childValidators) {
+        return new(function(args, childValidators) {
 
             var self = this;
             self.childValidators = childValidators;
             self.args = args;
 
-
             self.parse = function(data, key, first) {
                 //All validators should handle opt (optional)
-                var args = merge(self.args, {opt : false});
+                var args = merge(self.args, { opt: false });
                 var val = parserFunc.call(self, args, self.childValidators, data, key);
                 if(first && val !== null && typeof val == "object" && val.htDeleteKey) return null;
                 return val;
@@ -87,6 +94,7 @@ function makeParser(parserFunc, docFunc) {
             self.document = function() {
                 return docFunc.call(self, args);
             };
+
         })(args, childValidators);
 
     };
