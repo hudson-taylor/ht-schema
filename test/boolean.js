9c22eb5036f44818320b6152f07dc7dba775862e
Adam Brady <adam@boxxen.org>
2015-01-13 01:37:03 +1100

    remove commented out code
    Signed-off-by: Adam Brady <adam@boxxen.org>

--- test/boolean.js
+++ test/boolean.js
@@ -16,7 +16,7 @@ describe("Boolean validator", function() {
         });
 
     });
-    
+
     it("should allow optional value if opt is set", function() {
 
         var schema = s.Boolean({ opt: true });
@@ -60,31 +60,4 @@ describe("Boolean validator", function() {
 
     });
 
-    // it("should allow optional value", function() {
-    //     var schema = s.Boolean({ opt: true });
-    //     assert.doesNotThrow(function() {
-    //       schema.validate();
-    //     });
-    // });
-
-    // it("should accept a valid Bool", function() {
-    //     var schema = s.Boolean();
-    //     assert.equal(schema.validate(true), true);
-    // });
-
-    // it("should reject non-boolean", function() {
-    //     var schema = s.Boolean();
-    //     [ 42, "hello", /world/ ].forEach(function(s) {
-    //       assert.throws(function() {
-    //         schema.validate(s);
-    //       });
-    //     });
-    // });
-
-    // it("should coerce values with option set", function() {
-    //     var schema = s.Boolean({ coerce: true });
-    //     assert.equal(schema.validate("hello"), true);
-    //     assert.equal(schema.validate(""), false);
-    // });
-    
 });
