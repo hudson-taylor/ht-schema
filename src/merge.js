979be9c02305f21c68f9d2f7b372594e59b7bf32
Adam Brady <adam@boxxen.org>
2016-02-28 23:32:59 +1100

    Update project to use semistandard

--- src/merge.js
+++ src/merge.js
@@ -1,11 +1,11 @@
-function merge() {
+function merge () {
   // Merge objects passed as arguments, left to right precidence.
   let result = {};
   for (let i = 0; i < arguments.length; i++) {
     let keys = Object.keys(arguments[i]);
     for (let k = 0; k < keys.length; k++) {
       let key = keys[k];
-      if(!result.hasOwnProperty(key)) {
+      if (!result.hasOwnProperty(key)) {
         result[key] = arguments[i][key];
       }
     }
